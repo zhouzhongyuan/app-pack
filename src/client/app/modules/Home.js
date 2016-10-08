8b6f81b87550f783a0492d6ec1064bc3570895af
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-10-08 17:19:44 +0800

    style nav bar

--- src/client/app/modules/Home.js
+++ src/client/app/modules/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export default React.createClass({
-  render() {
-    return <div>Home</div>
-  }
+    render() {
+        return (
+            <div
+                style={{marginTop: 150, height: 2000}}
+            >Home
+            </div>
+        );
+    }
 })
