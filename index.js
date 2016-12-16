787d99446effee3ca910e32f6c960ca72b723f22
zhouzhongyuan <yawenzhongyuan@gmail.com>
2016-12-16 17:32:01 +0800

    login server back success

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
+const config = require('./config');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+// 连接数据库，require数据模型
+require('./server/models').connect(config.dbUri);
+
 const app = express()
 
 // 静态文件
 app.use(express.static('./server/static/'))
 app.use(express.static('./client/dist/'))
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// pass the passport middleware
+app.use(passport.initialize());
+
+// load passport strategies
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-login', localLoginStrategy);
 
 // 路由
 const authRoutes = require('./server/routes/auth.js');
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const passport = require('passport');
 
 const router = new express.Router();
 router.post('/login', (req, res, next) => {
-    res.json({success: true});
+    return passport.authenticate('local-login', (err, token, userData) => {
+        if (err) {
+            if (err.name === 'IncorrectCredentialsError') {
+                return res.status(400).json({
+                    success: false,
+                    message: err.message
+                });
+            }
+
+            return res.status(400).json({
+                success: false,
+                message: 'Could not process the form.'
+            });
+        }
+
+        return res.json({
+            success: true,
+            message: '登陆成功!',
+            token,
+            user: userData
+        });
+    })(req, res, next);
 });
 
 module.exports = router;
