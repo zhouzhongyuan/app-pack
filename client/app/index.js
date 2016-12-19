ee300d40f2b9378a54667486f02317a0c620c6e2
zhouzhongyuan <yawenzhongyuan@gmail.com>
2016-12-19 11:06:33 +0800

    if not login, redirect to login

--- client/app/index.js
+++ client/app/index.js
@@ -12,6 +12,16 @@ import App from './modules/App';
 import BasicSetting from './modules/app-setting/BasicSetting';
 import IconSetting from './modules/app-setting/Icon';
 injectTapEventPlugin();
+import Auth from './modules/Auth'
+
+function requireAuth(nextState, replace) {
+    if (!Auth.isUserAuthenticated()) {
+        replace({
+            pathname: '/login'
+        })
+    }
+}
+
 ReactDOM.render(
     <MuiThemeProvider>
         <Router history={browserHistory}>
@@ -20,7 +30,7 @@ ReactDOM.render(
                 <Route path="/about" component={About} />
                 <Route path="/doc" component={Doc} />
                 <Route path="/login" component={Login} />
-                <Route path="/app" component={App} >
+                <Route path="/app" component={App} onEnter={requireAuth} >
                     <IndexRoute component={BasicSetting} />
                     <Route path="basic" component={BasicSetting} />
                     <Route path="icon" component={IconSetting} />
--- client/app/modules/Auth.js
+++ client/app/modules/Auth.js
@@ -2,6 +2,9 @@ class Auth {
     static  authenticateUser(token){
         localStorage.setItem('token', token);
     }
+    static isUserAuthenticated(){
+        return localStorage.getItem('token') !== null;
+    }
 }
 
 export default Auth;
