d963fd05571103a563332629a7968646c8f99a5e
zhouzhongyuan <yawenzhongyuan@gmail.com>
2016-12-23 13:40:06 +0800

    添加对server端的babel转换支持

---
+++ webpack.browser.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'client/dist/js');
+var APP_DIR = path.resolve(__dirname, 'client/app');
+
+var config = {
+    entry: APP_DIR + '/index.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'app.js'
+    },
+    devtool:'eval-source-map',
+    module : {
+        loaders : [
+            {
+                test : /\.jsx?/,
+                include : APP_DIR,
+                loader : 'babel'
+            }
+        ]
+    },
+    watch: true
+};
+
+module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,6 @@
-var webpack = require('webpack');
-var path = require('path');
-
-var BUILD_DIR = path.resolve(__dirname, 'client/dist/js');
-var APP_DIR = path.resolve(__dirname, 'client/app');
-
-var config = {
-    entry: APP_DIR + '/index.js',
-    output: {
-        path: BUILD_DIR,
-        filename: 'app.js'
-    },
-    devtool:'eval-source-map',
-    module : {
-        loaders : [
-            {
-                test : /\.jsx?/,
-                include : APP_DIR,
-                loader : 'babel'
-            }
-        ]
-    },
-    watch: true
-};
-
-module.exports = config;
+const browserConfig = require('./webpack.browser.config');
+const serverConfig = require('./webpack.server.config');
+module.exports = [
+    Object.assign({} , browserConfig),
+    Object.assign({} , serverConfig),
+];
